1b849257914d77ceeca8a3d62c39a172c16d21e5
sibashis10 <siba.march10@gmail.com>
2021-12-26 18:09:08 +0530

    Update for combining first name and last name for Person entity

--- src/main/java/com/navikenz/codingexercises/util/NERModel.java
+++ src/main/java/com/navikenz/codingexercises/util/NERModel.java
@@ -21,15 +21,19 @@ public class NERModel {
 	public static Map<String, String> classifyNamedEntity(List<String> sentences) throws ClassCastException, ClassNotFoundException, IOException {
 		
 		String serializedClassifier = "classifiers/english.all.3class.distsim.crf.ser.gz";
-		AbstractSequenceClassifier<CoreLabel> classifier = CRFClassifier.getClassifier(serializedClassifier);
+		Properties props = new Properties();
+	    props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner,parse,dcoref");
+		AbstractSequenceClassifier<CoreLabel> classifier = CRFClassifier.getClassifier(serializedClassifier, props);
+		
 		Map<String, String> entityMap = new HashMap<>(); 
 		
 		sentences.stream().forEach(sentence -> {
-			String rawText = classifier.classifyToString(sentence, "tsv", false);
+			String rawText = classifier.classifyToString(sentence, "tabbedEntities", false);
+			System.out.println(rawText);
 			Map<String, String> eMap = Arrays.asList(rawText.split("\n"))
 				.stream()
 				.map(row -> row.split("\t"))
-				.filter(cell -> cell.length == 2 && !cell[1].trim().equals("O"))
+				.filter(cell -> cell.length > 2 && !cell[0].trim().equals("") && !cell[1].trim().equals(""))
 				.collect(Collectors.toMap(e -> e[0], e -> e[1]));
 			
 			entityMap.putAll(eMap);
@@ -41,7 +45,7 @@ public class NERModel {
 	public static List<String> sentenceSplitter(String text) {
 	    List<String> sentences = new ArrayList<>();
 		Properties props = new Properties();
-	    props.setProperty("annotators", "tokenize,ssplit");
+	    props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner,parse,dcoref");
 	    StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
 	    CoreDocument doc = new CoreDocument(text);
 	    pipeline.annotate(doc);
@@ -50,5 +54,11 @@ public class NERModel {
 	    	sentences.add(sent.text());
 	    }
 	    return sentences;
+	}
+	
+	public static void main(String[] args) throws ClassCastException, ClassNotFoundException, IOException {
+		List<String> sentences = new ArrayList<>();
+		sentences.add("Working as a chef, would love to have her own restaurant one day, has spent 2 years at Javu and Alessandros, sister of Ross Geller, married to Chandler Bing, was Rachel Greene's roommate for a long time.");
+		System.out.println(classifyNamedEntity(sentences));
 	}
 }
